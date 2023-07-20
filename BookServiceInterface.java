75a4e4374e91430d9f6ed361fd96dab151f09ef1
pradeepa1coder <131429459+pradeepa1coder@users.noreply.github.com>
2023-07-20 11:05:11 +0530

    Add files via upload

---
+++ BookServiceInterface.java
@@ -0,0 +1,12 @@
+package org.proj2;
+public interface BookServiceInterface {
+    void addBook();
+
+    void showAllBooks();
+
+    void showAllAvailableBooks();
+
+    void borrowBook();
+
+    void returnBook();
+}
