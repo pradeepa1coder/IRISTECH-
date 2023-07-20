776369e4c81bc31331e06ebde2c0d8ea7f2a1a00
pradeepa1coder <131429459+pradeepa1coder@users.noreply.github.com>
2023-07-20 11:09:46 +0530

    Add files via upload
    latest

--- Main.java
+++ Main.java
@@ -1,44 +1,76 @@
-package org.proj2;
+package com.sjprogramming.quizapp;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BookServiceInterface service = new BookServiceImpl();
+    public static void main(String[] args) {
+        Quiz q=new Quiz();
+        q.logic();
 
-		do {
-			System.out.println("Welcome to Book Management Application");
-			System.out.println("1.Add Book\n" + "2.Show All Books\n" + "3.Show Available Books\n" + "4.Borrow Book\n"
-					+ "5.Return Book\n" + "6.Exit\n");
-
-			System.out.println("Enter Your Choice !");
-			int ch = sc.nextInt();
-
-			switch (ch) {
-			case 1:
-				service.addBook();
-				break;
-			case 2:
-				service.showAllBooks();
-				break;
-			case 3:
-				service.showAllAvailableBooks();
-				break;
-			case 4:
-				service.borrowBook();
-				break;
-			case 5:
-				service.returnBook();
-				break;
-			case 6:
-				System.out.println("Thank you for Using Application !!");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Please Enter Valid Choice !");
-
-			}
-		} while (true);
-	}
+    }
 }
+
+class Quiz{
+    Scanner sc=new Scanner(System.in);
+    int correctAnsCount=0;
+    int wrongAnsCount=0;
+    public void logic(){
+        Questions q1 = new Questions(" Which statement is true about Java?", "A. Java is a sequence-dependent programming language ", "B. Java is a code dependent programming language ", "C. Java is a platform-dependent programming language ", "D. Java is a platform-independent programming language ");
+        Questions q2 = new Questions("What is the extension of java code files?", " A. .txt", "B. .pdf", "C. .sql", "D. .java");
+        Questions q3 = new Questions("Who invented Java Programming? ", "A. Guido van Rossum", "B. James Gosling", "C. Dennis Ritchie", "D. Bjarne Stroustrup");
+        Questions q4 = new Questions("Which one of the following is not a Java feature?", "A. Object-oriented", "B. Use of pointers", "C. Portable", "D. Dynamic and Extensible");
+        Questions q5 = new Questions("Which of these cannot be used for a variable name in Java?", "A. identifier & keyword", "B. identifier", "C. Keyword", "D. none of the mentioned");
+
+        Map<Questions,Character> hmap=new HashMap<>();
+        hmap.put(q1,'D');
+        hmap.put(q2,'D');
+        hmap.put(q3,'B');
+        hmap.put(q4,'B');
+        hmap.put(q5,'C');
+
+        for(Map.Entry<Questions,Character> map:hmap.entrySet()){
+            System.out.println(map.getKey().getQuestion());
+            System.out.println(map.getKey().getOption1());
+            System.out.println(map.getKey().getOption2());
+            System.out.println(map.getKey().getOption3());
+            System.out.println(map.getKey().getOption4());
+
+            System.out.println("Enter Your Answer: ");
+            Character ans=sc.next().charAt(0);
+
+            int cans=Character.compare(ans,map.getValue());
+            if(cans==0){
+                System.out.println("Correct");
+                correctAnsCount++;
+            }
+            else{
+                System.out.println("Wrong");
+                wrongAnsCount++;
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("--------Result---------");
+        System.out.println("Total Questions: "+hmap.size());
+        System.out.println("Correct Answered Questions : "+correctAnsCount);
+        System.out.println("Wrong Answered Questions : "+wrongAnsCount);
+        int percentage=(100*correctAnsCount)/hmap.size();
+        System.out.println("Percentage : "+percentage);
+        if(percentage>95){
+            System.out.println("Performance : Very Good");
+        }
+        else if(percentage<35){
+            System.out.println("Performance : Very Low");
+        }
+        else{
+            System.out.println("Performance : Medium");
+        }
+
+
+    }
+}
+
